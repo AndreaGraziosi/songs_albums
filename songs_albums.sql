5c64aa30e2eb572f1bfdb5d3439eb208d84c22a8
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-01-28 11:55:58 -0800

    removed extra semicolon

--- songs_albums.sql
+++ songs_albums.sql
@@ -71,19 +71,19 @@ JOIN Songs ON Albums.id = Songs.album_id;
  */
 SELECT name
 FROM Albums
-WHERE year_published > 1970 AND Albums.year_published < 1980;
+WHERE year_published > 1970 AND year_published < 1980;
 /*
  * TODO: Find all songs on albums published between 1970 and 1980. 
  *(Hint: Use a table join.)
  */
  SELECT Songs.song_name
  FROM Albums
- JOIN Songs ON Albums.id = Songs.album_id;
+ JOIN Songs ON Albums.id = Songs.album_id
  WHERE Albums.year_published > 1970 AND Albums.year_published < 1980;
 /*
  * TODO: Find all songs on albums with names containing 'California'.
  */
  SELECT Songs.song_name
  FROM Albums
- JOIN Songs ON Albums.id = Songs.album_id;
+ JOIN Songs ON Albums.id = Songs.album_id
  WHERE Albums.name LIKE '%CALIFORNIA%';
